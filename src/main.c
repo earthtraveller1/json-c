556202f1816eaf403390cf6b8bbbbdee062a62f4
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-08-22 12:05:31 -0400

    Tested the file size reader.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
+#include "file-utils.h"
+
 int main()
 {
-    printf("Hello World!\n");
+    FILE* test_file = fopen("sandbox/test.txt", "r");
+    if (test_file == NULL)
+    {
+        fprintf(stderr, "[FATAL ERROR]: Failed to open sandbox/test.txt\n");
+    }
+    
+    const uint32_t file_size = get_file_size(test_file);
+    
+    printf("File size: %d\n", file_size);
+    
     return 0;
 }
