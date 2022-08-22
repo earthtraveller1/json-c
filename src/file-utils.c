fd05e1185dedb176ac2e261afe26b5d58ffa395d
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-08-22 13:26:05 -0400

    Added function to read the contents of a file into a string.

--- src/file-utils.c
+++ src/file-utils.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 uint32_t get_file_size(FILE* p_file)
 {
@@ -13,4 +14,21 @@ uint32_t get_file_size(FILE* p_file)
     }
     
     return result;
+}
+
+const char* read_file_as_string(const char* p_filename)
+{
+    FILE* file = fopen(p_filename, "r");
+    if (!file)
+    {
+        return NULL;
+    }
+    
+    uint32_t buffer_length = get_file_size(file) + 1;
+    char* buffer = malloc((buffer_length) * sizeof(char));
+    
+    fseek(file, 0, SEEK_SET);
+    fgets(buffer, buffer_length, file);
+    
+    return buffer;
 }
--- src/file-utils.h
+++ src/file-utils.h
@@ -4,6 +4,8 @@
 #include <stdint.h>
 #include <stdio.h>
 
-uint32_t get_file_size(FILE* p_file);
+uint32_t get_file_size(FILE* file);
+
+const char* read_file_as_string(const char* filename);
 
 #endif /* CBF5C40D_B72C_4B47_8BAF_3C807BA246A8 */
