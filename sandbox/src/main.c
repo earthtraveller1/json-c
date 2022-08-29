71256d6adc6a5d8efce3018fb7de928b2437fd7e
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-08-29 12:11:13 -0400

    Tested the function written in the last commit.

--- sandbox/src/main.c
+++ sandbox/src/main.c
@@ -1,7 +1,23 @@
+#include <file-utils.h>
+#include <json.h>
+#include <parser.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-    printf("Hello, World!\n");
+int main() {
+    const char* file_contents = read_file_as_string("sandbox/tony.json");
+    
+    uint32_t token_count;
+    const char** tokens = tokenize_string(file_contents, &token_count);
+    free((void*)file_contents);
+    
+    uint32_t symbol_count;
+    const char** symbols = chunk_tokens_to_symbols(tokens, token_count, &symbol_count);
+    free_string_array(tokens, token_count);
+    
+    struct json_object json_object = json_parse_object(symbols, symbol_count);
+    
+    printf("Number of fields: %ld", json_object.number_of_fields);
+    
     return 0;
 }
