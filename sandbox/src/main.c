4e655f294b053ca53963a64c36cdb2182c7e68b2
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-08-30 17:56:00 -0400

    Silenced a small warning and formatted the code.

--- sandbox/src/main.c
+++ sandbox/src/main.c
@@ -4,20 +4,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    const char* file_contents = read_file_as_string("sandbox/tony.json");
-    
+int main()
+{
+    const char *file_contents = read_file_as_string("sandbox/tony.json");
+
     uint32_t token_count;
-    const char** tokens = tokenize_string(file_contents, &token_count);
-    free((void*)file_contents);
-    
+    const char **tokens = tokenize_string(file_contents, &token_count);
+    free((void *)file_contents);
+
     uint32_t symbol_count;
-    const char** symbols = chunk_tokens_to_symbols(tokens, token_count, &symbol_count);
+    const char **symbols =
+        chunk_tokens_to_symbols(tokens, token_count, &symbol_count);
     free_string_array(tokens, token_count);
-    
+
     struct json_object json_object = json_parse_object(symbols, symbol_count);
-    
-    printf("Number of fields: %ld", json_object.number_of_fields);
-    
+
+    printf("Number of fields: %zd", json_object.number_of_fields);
+
     return 0;
 }
