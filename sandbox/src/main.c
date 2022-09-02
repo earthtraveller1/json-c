25583757894aa9f92a1173717bc397478ef42fe0
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-09-02 17:58:37 -0400

    Changed the focus of the test.

--- sandbox/src/main.c
+++ sandbox/src/main.c
@@ -10,31 +10,38 @@ int main()
 
     uint32_t token_count;
     const char **tokens = tokenize_string(file_contents, &token_count);
+    
+    printf("[INFO]: Tokens:\n");
+    for (uint32_t i = 0; i < token_count; i++)
+    {
+        printf("\t%s\n", tokens[i]);
+    }
+    
     free((void *)file_contents);
     
-    uint8_t status;
-    struct json_object json_object = json_parse_object(tokens, token_count, &status);
-    if (!status)
-    {
-        fprintf(stderr, "[ERROR]: Failed to parse JSON 'sandbox/tony.json'\n");
-        return -1;
-    }
+    // uint8_t status;
+    // struct json_object json_object = json_parse_object(tokens, token_count, &status);
+    // if (!status)
+    // {
+    //     fprintf(stderr, "[ERROR]: Failed to parse JSON 'sandbox/tony.json'\n");
+    //     return -1;
+    // }
 
-    printf("[INFO]: Number of fields: %zd\n", json_object.number_of_fields);
+    // printf("[INFO]: Number of fields: %zd\n", json_object.number_of_fields);
     
-    printf("[INFO]: Field names and values:\n");
-    for (uint32_t i = 0; i < json_object.number_of_fields; i++)
-    {
-        printf("\t%s = ", json_object.fields[i].name);
-        if (json_object.fields[i].type == JSON_FIELD_TYPE_STRING)
-        {
-            printf("%s\n", json_object.fields[i].value.string_value);
-        }
-        else 
-        {
-            printf("<NOT STRING>\n"); // Sorry, too lazy
-        }
-    }
+    // printf("[INFO]: Field names and values:\n");
+    // for (uint32_t i = 0; i < json_object.number_of_fields; i++)
+    // {
+    //     printf("\t%s = ", json_object.fields[i].name);
+    //     if (json_object.fields[i].type == JSON_FIELD_TYPE_STRING)
+    //     {
+    //         printf("%s\n", json_object.fields[i].value.string_value);
+    //     }
+    //     else 
+    //     {
+    //         printf("<NOT STRING>\n"); // Sorry, too lazy
+    //     }
+    // }
 
     return 0;
 }
